01eff25a6ad45465ba9db14ed45d065046de8db7
User <user@Users-MacBook-Pro.local>
2025-05-09 22:23:30 +0530

    added class diagram

--- Factory Design Pattern/src/Main.java
+++ Factory Design Pattern/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Factory Design Pattern: vehicles.Vehicle Factory");
         VehicleFactory vehicleFactory = VehicleFactory.getInstance();
-        Vehicle myVehicle = vehicleFactory.getVehicle(VehicleType.BUS);
+        Vehicle myVehicle = vehicleFactory.getVehicle(VehicleType.CAR);
         myVehicle.drive();
     }
 }
